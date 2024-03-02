8293e8ac01e81e440c5beeab758d985d9a3808eb
DolceCasa <103113557+li-peifeng@users.noreply.github.com>
2024-03-03 00:34:38 +0100

    upload dev dist

---
+++ dist/assets/index.57b83d1c.js
@@ -0,0 +1,1 @@
+import{c as t,a9 as e,a6 as r}from"./index.51a697c4.js";import n from"./Upload.f30a6356.js";import"./index.6099bf5b.js";const m=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{m as default};
