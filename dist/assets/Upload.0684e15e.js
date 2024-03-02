e3a465543e0ea99de28d4ecca306bead37f2b81f
DolceCasa <103113557+li-peifeng@users.noreply.github.com>
2024-03-03 00:34:38 +0100

    upload dev dist

---
+++ dist/assets/Upload.0684e15e.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index.109b1195.js";import{g as a}from"./useTitle.39061f68.js";import{T as e}from"./Tasks.9419ae8f.js";import"./Paginator.3eaa01f1.js";import"./index.5ed7d4d0.js";const i=()=>(a("manage.sidemenu.upload"),o(e,{type:"upload"}));export{i as default};
