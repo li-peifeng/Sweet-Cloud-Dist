c949a690eacfcb4a35246bbfeb64d37209fb37e5
Noah Hsu <i@nn.ci>
2024-09-10 22:25:30 +0000

    upload 3.37.1 dist files

---
+++ dist/assets/Upload.b0b83ddf.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index.0796fe68.js";import{g as a}from"./useTitle.ef99e565.js";import{T as e}from"./Tasks.54e2a751.js";import"./Paginator.04f7e410.js";import"./index.a998bc95.js";const i=()=>(a("manage.sidemenu.upload"),o(e,{type:"upload"}));export{i as default};
