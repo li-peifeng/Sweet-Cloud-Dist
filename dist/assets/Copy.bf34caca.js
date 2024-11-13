978f2daf24f43e33cf60cde667ad342db4c375bd
Noah Hsu <i@nn.ci>
2024-11-13 16:30:50 +0000

    upload 3.39.1 dist files

--- dist/assets/Copy.bf34caca.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index.109b1195.js";import{g as o}from"./useTitle.39061f68.js";import{T as t}from"./Tasks.9419ae8f.js";import"./Paginator.3eaa01f1.js";import"./index.5ed7d4d0.js";const n=()=>(o("manage.sidemenu.copy"),e(t,{type:"copy",canRetry:!0}));export{n as default};
--- dist/assets/index.e7eda48a.js
+++
@@ -1,1 +0,0 @@
-import{c as t,a9 as e,a6 as r}from"./index.109b1195.js";import n from"./Upload.694e03d3.js";import"./index.21edcee1.js";const m=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{m as default};
